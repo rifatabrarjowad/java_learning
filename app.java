1683f662bac71f47228ca587851aa140bf512dd3
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-17 23:07:38 +0600

    hello world!

---
+++ app.java
@@ -0,0 +1,19 @@
+public class hello {
+    public static void main(String[] args) {
+        int a = 80;
+        int b = 70;
+
+        int c,d;
+        c = 5 ;
+        d = 6 ;
+
+        String x = "Rifat" ;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(x);
+
+
+    }
+}
