bb624c3f3a23915682ef42e0afa2703d264e79dc
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-20 03:02:30 +0600

    adding file

---
+++ assi-01/src/possneg.java
@@ -0,0 +1,18 @@
+public class possneg {
+    public static void main(String[] args)
+    {
+        int number=109;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive or negative");
+        }
+    }
+}
