2411cac748f110948aa3502ffdde661a4df4c27c
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-20 01:48:07 +0600

    odd number print

---
+++ assi-01/src/helloWorld.java
@@ -0,0 +1,10 @@
+// Your First Program
+
+class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("odd numbers print : ");
+        for(int i=2; i<100; i++)
+            if (i%2!=0)
+                System.out.println(" "+i);
+    }
+}
